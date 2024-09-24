4390190192a70d9bad19603ea0b2889f220db4b4
jaisleek <babajide.babalola@yahoo.com>
2024-09-24 19:49:22 +0100

    Initial commit

---
+++ src/layouts/ReviewCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BsStarFill, BsStarHalf } from 'react-icons/bs'
+import { FaQuoteRight } from 'react-icons/fa'
+
+const ReviewCard = (props) => {
+    return (
+        <div className='flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5'>
+            <div className='flex flex-row items-center lg:justify-start justify-center'>
+                <div className='w-1/4'>
+                    <img className='rounded-full' src={props.img} alt="img" />
+                </div>
+                <div className='w-1/4'>
+                    <h2 className='font-semibold text-lg'>{props.title}</h2>
+                    <div className='flex'>
+                        <BsStarFill className='text-brightColor' />
+                        <BsStarFill className='text-brightColor' />
+                        <BsStarFill className='text-brightColor' />
+                        <BsStarFill className='text-brightColor' />
+                        <BsStarHalf className='text-brightColor' />
+                    </div>
+                </div>
+                <span className='ml-16'>
+                    <FaQuoteRight className="text-backgroundColor" size={42} />
+                </span>
+            </div>
+            <p>
+                lorem ipsum dolor sit amet consectetur adipisicing elit. Laudanatium ab
+                et optio labore perferebdis saepe, Fuga, delenti Laudanatium eos
+                delectus cumque repudiantes ut deserunt 1d7
+            </p>
+        </div>
+    )
+}
+
+export default ReviewCard
